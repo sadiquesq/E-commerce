becd1c71b096f8954b0ff9b1ff2381d2cecfd46c
sadiquesq <sadiquesanu1@gmail.com>
2025-02-27 14:38:35 +0530

    thrid

--- E-CommerBackEnd/Program.cs
+++ E-CommerBackEnd/Program.cs
@@ -101,7 +101,7 @@ namespace E_Commerce
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction()) 
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
